1298616715e04965067fb66109c6cd3723d0ff88
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-05-09 11:03:29 +0000

    3

--- datatype.js
+++ datatype.js
@@ -1,13 +1,23 @@
-"use strict";
+// "use strict";
 
-let name = "ronik";
-let age = 24;
-let isLoggedIn =false
+// let name = "ronik";
+// let age = 24;
 
+// let score = "ronik";
+// console.log(typeof score);
 
-let score = "33"
-console.log(typeof(score));
+// let valueInNuber = Number(score);
+// console.log(typeof valueInNuber);
+// console.log(valueInNuber);
 
-let valueInNuber = Number(score)
-console.log(typeof(score));
+// let isLoggedIn = "heeed";
 
+// let boolearIsLD = Boolean(isLoggedIn);
+
+// console.log(boolearIsLD);
+
+let somenumbr=33
+
+let stringNumber =String(somenumbr)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
