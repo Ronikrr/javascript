43bec845e68c9efbe6cd414dd0fabdbdc8987ff3
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-05-09 09:31:13 +0000

    dtata

---
+++ datatype.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let name = "ronik";
+let age = 24;
+let isLoggedIn =false
+
+
+console.log(typeof undefined);
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("ronik")
+const accountId = 1423322
+let Email ="Ronikgor@gmail.com"
+var pass = "123"
+accountcity="surat"
+
+let accoutState
+
+// accountId = 5 
+Email = "ronikgor@"
+pass = "2121212"
+accountcity="guj"
+
+/*
+
+*/
+console.table([accountId,Email,pass,accountcity,accoutState])
