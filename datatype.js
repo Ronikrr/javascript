7bb05c2fbd0a18e88806d23af746dc845ba099ba
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-05-10 05:44:31 +0000

    eo-2

--- datatype.js
+++ datatype.js
@@ -19,5 +19,27 @@
 let somenumbr=33
 
 let stringNumber =String(somenumbr)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+//*************************************************Oprations  */
+
+
+let value = -3
+// console.log(value);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let sttr1 ="hello"
+let sttr2 =" ronik"
+
+let str=sttr1+sttr2
+// console.log(str);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log((1+2)+"2"+2+5);
+
+// console.log(+"");
